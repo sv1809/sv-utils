0e617cf0105ce594e06d22ce799a99cc8706336c
sv1809 <samorukov.v@sovzond.center>
2016-12-06 14:55:55 +0500

    add setValue method

--- src/object-utils.js
+++ src/object-utils.js
@@ -2,16 +2,50 @@
  * Find object property by string, e.g. 'prop1.prop2.prop3'
  * Return undefined if property not found
  */
-export function findProperty(obj, property) {
+export function getValue(obj, property) {
     if (obj == null || property == null) {
         return;
     }
-    var arr = property.split(".");
+    const arr = property.split(".");
     while (arr.length && (obj = obj[arr.shift()])) {
     }
     return obj;
 }
 
+export function setValue(obj, property, newValue) {
+    if (obj == null) {
+        obj = {};
+    }
+    const ind = property.indexOf('.');
+    const testArray = /^(.+)\[(\d+)\]$/;
+    const current = ind !== -1 ? property.substring(0, ind) : property;
+    const matches = testArray.exec(current);
+    if (matches) {
+        const arrInd = matches[2];
+        const prop = matches[1];
+        const res = obj[prop].map((curr, i) => {
+            if (i == arrInd) {
+                if (ind === -1) {
+                    if (typeof curr === 'object') {
+                        return Object.assign({}, curr, newValue);
+                    } else {
+                        return newValue;
+                    }
+                } else {
+                    return setValue(curr, property.substring(ind + 1), newValue);
+                }
+            }
+            return curr;
+        });
+        return Object.assign({}, obj, { [prop]: res });
+    }
+    if (ind === -1) {
+        return Object.assign({}, obj, { [property]: newValue });
+    }
+    return Object.assign({}, obj, { [current]: setValue(obj[current], property.substring(ind + 1), newValue) });
+}
+
 export default {
-    findProperty
+    getValue,
+    setValue,
 }
--- tests/object-utils.spec.js
+++ tests/object-utils.spec.js
@@ -18,49 +18,140 @@ describe('Object utils', () => {
         done();
     });
 
-    describe('findProperty', () => {
+    describe('getValue', () => {
 
         it('it should return value when find property', (done) => {
-            const res = objectUtils.findProperty(testObject, 'prop1.prop1_3.prop1_3_1');
+            const res = objectUtils.getValue(testObject, 'prop1.prop1_3.prop1_3_1');
             assert.equal(res, '1.3.1');
             done();
         });
 
         it('it should return bull when find property with null value', (done) => {
-            const res = objectUtils.findProperty(testObject, 'prop1.prop1_1');
+            const res = objectUtils.getValue(testObject, 'prop1.prop1_1');
             assert.equal(res, null);
             done();
         });
 
         it('it should return undefined when can\'t find property', (done) => {
-            const res = objectUtils.findProperty(testObject, 'prop1.prop');
+            const res = objectUtils.getValue(testObject, 'prop1.prop.prop3');
             assert.equal(res, undefined);
             done();
         });
 
         it('it should return undefined when object is null', (done) => {
-            const res = objectUtils.findProperty(null, 'prop1.prop');
+            const res = objectUtils.getValue(null, 'prop1.prop');
             assert.equal(res, undefined);
             done();
         });
 
         it('it should return undefined when object is undefined', (done) => {
-            const res = objectUtils.findProperty(undefined, 'prop1.prop');
+            const res = objectUtils.getValue(undefined, 'prop1.prop');
             assert.equal(res, undefined);
             done();
         });
 
         it('it should return undefined when property is null', (done) => {
-            const res = objectUtils.findProperty(testObject, null);
+            const res = objectUtils.getValue(testObject, null);
             assert.equal(res, undefined);
             done();
         });
 
         it('it should return undefined when property is undefined', (done) => {
-            const res = objectUtils.findProperty(testObject, undefined);
+            const res = objectUtils.getValue(testObject, undefined);
             assert.equal(res, undefined);
             done();
         });
+
+    })
+
+    describe('setValue', () => {
+
+        it('it should set object property', done => {
+            const property = 'prop1.prop1_1';
+            const newValue = 5;
+            const obj = {
+                prop1: {
+                    prop1_1: 2
+                }
+            };
+            const expected = {
+                prop1: {
+                    prop1_1: 5
+                }
+            };
+            const res = objectUtils.setValue(obj, property, newValue);
+            assert.equal(JSON.stringify(res), JSON.stringify(expected));
+            done();
+        })
+
+        it('it should set object property in array', done => {
+            const property = 'prop1.prop1_1[0]';
+            const newValue = 5;
+            const obj = {
+                prop1: {
+                    prop1_1: [2, 4]
+                }
+            };
+            const expected = {
+                prop1: {
+                    prop1_1: [5, 4]
+                }
+            };
+            const res = objectUtils.setValue(obj, property, newValue);
+            assert.equal(JSON.stringify(res), JSON.stringify(expected));
+            done();
+        })
+
+        it('it should create object property if not exist', done => {
+            const property = 'prop1.prop1_1.prop1_1_1.prop1_1_1_1';
+            const newValue = 5;
+            const obj = {
+                prop2: {
+                    prop2_1: 2
+                }
+            };
+            const expected = {
+                prop2: {
+                    prop2_1: 2
+                },
+                prop1: {
+                    prop1_1: {
+                        prop1_1_1: {
+                            prop1_1_1_1: 5
+                        }
+                    }
+                },
+            };
+            const res = objectUtils.setValue(obj, property, newValue);
+            assert.equal(JSON.stringify(res), JSON.stringify(expected));
+            done();
+        })
+
+        it('it should set object property in object array', done => {
+            const property = 'prop1.prop1_1[0].arrProp1.arrProp1_1';
+            const newValue = 5;
+            const obj = {
+                prop1: {
+                    prop1_1: [{
+                        arrProp1: {
+                            arrProp1_1: 2
+                        }
+                    }, 4]
+                }
+            };
+            const expected = {
+                prop1: {
+                    prop1_1: [{
+                        arrProp1: {
+                            arrProp1_1: 5
+                        }
+                    }, 4]
+                }
+            };
+            const res = objectUtils.setValue(obj, property, newValue);
+            assert.equal(JSON.stringify(res), JSON.stringify(expected));
+            done();
+        })
 
     })
 
